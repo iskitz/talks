51bbcf177b8bf354747e27b4e3b1bef5690d0933
Mike Lee <iskitz@users.noreply.github.com>
2020-05-07 03:13:01 -0700

    2020: tba: added bio for engineering-focused talk

---
+++ 2020/tba/mike.lee.bio.js
@@ -0,0 +1,29 @@
+;
+~
+{ re:
+    { id: 'tba.bio@mike.iskitz.net'
+    , by: 'mike.lee'
+    , on: -7.202005050500
+    , to: -7.202005070309
+    , at: +0.01
+    , in: 'san-jose.california.us.earth'
+    , is: "Mike Lee's Biography for a TBA engineering talk"
+    , it:
+        [ "describes Mike Lee's engineering career, interest & current focus"
+        , "highlights similarities between Mike & the audience's demograhics"
+        ]
+    , we:
+        [ "were ..."
+        , "must ..."
+        , "will ..."
+        , "like ..."
+        , "wont ..."
+        ]
+    },
+
+  bio:
+    /Mike Lee's an experienced engineering leader who's contributed to and helped shape the software industry's technology & people for over 20 years. He's built & led teams at Netflix, Khan Academy, Mozilla, Amazon, HP & Palm, and contributed to others at Canon, Sybase & UTC. He was born in Guyana, South America and now lives in the USA.//
+
+    /Mike's passionate about software development and enjoys exploring, discovering & sharing the capabilities & applications of languages like JavaScript & Python to improve understanding & delight. He's currently focused on the development of ionify, an API for describing, developing & discovering discrete units of data & behavior./
+}
+;
